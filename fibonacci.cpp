6b196eb86be926f4e8408f8701581d01e9937f2b
mithun-shinde <mithun.codes@gmail.com>
2021-02-21 16:54:44 +0530

    first commit

---
+++ fibonacci.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+// series - 1 1 2 3 5 8 13 21 ...
+
+int main()
+{
+    int lastNum{200};
+    int num{1}, prevNum{0}, nextNum{0};
+    
+    while (1)
+    {
+        nextNum = num + prevNum;
+        
+        if(nextNum > lastNum)
+            break;
+        
+        cout << nextNum << " ";
+        prevNum = num;
+        num = nextNum;
+    }
+}
+
+
